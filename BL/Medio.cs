27950eb17e39a44707810ba2b4f3eafa70e8c58e
“Soporte <soporte@bstmexico.com>
2023-12-27 01:46:40 -0600

    Primer cambio en BL

---
+++ BL/Medio.cs
@@ -0,0 +1,14 @@
+﻿namespace BL
+{
+    public class Medio
+    {
+
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+
+            return result;
+        }
+
+    }
+}
