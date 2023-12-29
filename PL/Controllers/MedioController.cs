fca7f8790530e1c8d6f0a828b68cd86db618da5f
“Soporte <soporte@bstmexico.com>
2023-12-29 01:42:53 -0600

    Se agrego un metodo al controlador y se modifico un metodo en la capa BL de Medio

--- PL/Controllers/MedioController.cs
+++ PL/Controllers/MedioController.cs
@@ -5,9 +5,18 @@ namespace PL.Controllers
 {
     public class MedioController : Controller
     {
+        [HttpGet]
        public IActionResult GetAll()
         {
             return View();
+        }
+
+
+        public JsonResult MedioGetById(int idMedio)
+        {
+            ML.Result result = BL.Medio.GetById(idMedio);
+
+            return Json(result);
         }
     }
 }
