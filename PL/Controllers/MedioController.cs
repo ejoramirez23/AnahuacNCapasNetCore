2d7a1a41e2844b5976cfd0825bb7f6f57ea70cc0
“Soporte <soporte@bstmexico.com>
2023-12-29 01:13:14 -0600

    Creacion del contrlador Medio

---
+++ PL/Controllers/MedioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class MedioController : Controller
+    {
+       public IActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
