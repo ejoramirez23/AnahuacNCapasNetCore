9b92a0f9eda15d7d9c727713db0622f2fbb3d00f
Eduardo Joel Ramirez <ejoramirez23@gmail.com>
2024-01-04 22:18:34 -0600

    Se agrego capa Service Layer

---
+++ SL/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SL/Model/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace SL.Model
+{
+    public class Usuario
+    {
+
+        public string Email { get; set; }
+        public string Pass { get; set; }
+
+    }
+}
